016d6b22c5bac29141ef5cf3b6306f3722402211
Yi Cheng <0418yicheng@gmail.com>
2023-12-08 18:27:35 -0500

    Cleaned up SwerveDriveCommand

--- src/main/java/frc/robot/commands/SwerveDriveCommand.java
+++ src/main/java/frc/robot/commands/SwerveDriveCommand.java
@@ -1,90 +1,53 @@
 package frc.robot.commands;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
-import frc.robot.RobotContainer;
+import frc.robot.lib.util.JoystickValues;
 import frc.robot.subsystems.joystick.JoystickSubsystem;
 import frc.robot.subsystems.drive.SwerveDriveSubsystem;
 
 public class SwerveDriveCommand extends CommandBase {
-    boolean squareUp;
-    PIDController controller;
-    boolean squareUpSingleStation;
-
     public SwerveDriveCommand() {
         addRequirements(
             SwerveDriveSubsystem.getInstance(),
             JoystickSubsystem.getInstance()
         );
 
-        controller = new PIDController(1.2, 0, 0.01);
-        controller.setSetpoint(0);
-        controller.setTolerance(Constants.TAU/100);
-        controller.enableContinuousInput(-Constants.TAU/2, Constants.TAU/2);
-
-        squareUp = false;
     }
 
     @Override
     public void initialize() {
+        //Resets all the wheels to face forwards
         SwerveDriveSubsystem.getInstance().resetModulesAbsolute();
     }
 
     @Override
     public void execute() {
-        // if(RobotContainer.operatorController.button(2).getAsBoolean() && RobotContainer.operatorController.button(7).getAsBoolean()){
-        //     if(DriverStation.getAlliance().get() == Alliance.Red)
-        //         controller.setSetpoint(
-        //             -Constants.TAU/4
-        //         );
-        //     else
-                controller.setSetpoint(
-                    Constants.TAU/4
-                );
-        //     squareUpSingleStation = true;
-        // }
+        //Takes in and processes the Joystick input for drive movement
+        JoystickValues moveInput = JoystickSubsystem.getInstance().getLeftJoystickValues().shape(
+            Constants.Joystick.MOVE_DEAD_ZONE,
+            Constants.Joystick.MOVE_SENSITIVITY
+            )
+            .swap()
+            .applyAngleDeadzone(Constants.Joystick.ANGLE_DEAD_ZONE);
 
-        // if(squareUp) {
-        //     SwerveDriveSubsystem.getInstance().set(
-        //         JoystickSubsystem.getInstance().getLeftJoystickValues().shape(
-        //             Constants.Joystick.MOVE_DEAD_ZONE,
-        //             Constants.Joystick.MOVE_SENSITIVITY
-        //         ).swap().applyAngleDeadzone(Constants.Joystick.ANGLE_DEAD_ZONE),
-        //         -controller.calculate(SwerveDriveSubsystem.getInstance().getGyroAngle())
-        //         );
-        // } else if(squareUpSingleStation) {
-        //     SwerveDriveSubsystem.getInstance().set(
-        //         JoystickSubsystem.getInstance().getLeftJoystickValues().shape(
-        //             Constants.Joystick.MOVE_DEAD_ZONE,
-        //             Constants.Joystick.MOVE_SENSITIVITY
-        //         ).swap().applyAngleDeadzone(Constants.Joystick.ANGLE_DEAD_ZONE),
-        //         -controller.calculate(SwerveDriveSubsystem.getInstance().getGyroAngle())
-        //         );
-        // } else {
-            SwerveDriveSubsystem.getInstance().set(
-                JoystickSubsystem.getInstance().getLeftJoystickValues().shape(
-                    Constants.Joystick.MOVE_DEAD_ZONE,
-                    Constants.Joystick.MOVE_SENSITIVITY
-                ).swap().applyAngleDeadzone(Constants.Joystick.ANGLE_DEAD_ZONE),
-                JoystickSubsystem.getInstance().getRightJoystickValues().shape(
-                    Constants.Joystick.TURN_DEAD_ZONE,
-                    Constants.Joystick.TURN_SENSITIVITY
-                ).x
-                );
-        // }
+        //Takes in and processes the Joystick input for the rotational movement (spinning)
+        double rotationInput = JoystickSubsystem.getInstance().getRightJoystickValues().shape(
+            Constants.Joystick.TURN_DEAD_ZONE,
+            Constants.Joystick.TURN_SENSITIVITY
+        ).x;
 
-        // if(controller.atSetpoint() || JoystickSubsystem.getInstance().getRightJoystickValues().x > 0.5){
-        //     squareUp = false;
-        //     squareUpSingleStation = false;
-        // } 
+        //Tells the Swerve Drive what to do
+        SwerveDriveSubsystem.getInstance().set(
+            moveInput,
+            rotationInput
+        );
+
     }
 
     @Override
     public void end(boolean interrupted) {
-        // SwerveDriveSubsystem.getInstance().set(0, 0, 0);
+
     }
  
     @Override
